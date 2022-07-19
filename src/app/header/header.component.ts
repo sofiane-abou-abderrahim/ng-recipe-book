d595ba760b04d95c44218784a2649d0f6530baef
SofianeAbouSafwan <sofiane.abou.safwan@gmail.com>
2022-07-19 18:41:22 +0100

    collapsable navigation bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,21 +9,20 @@ import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+  collapsed = true;
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(
-    private store: Store<fromApp.AppState>
-  ) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
     this.userSub = this.store
       .select('auth')
-      .pipe(map(authState => authState.user))
-      .subscribe(user => {
+      .pipe(map((authState) => authState.user))
+      .subscribe((user) => {
         this.isAuthenticated = !!user;
         console.log(!user);
         console.log(!!user);
